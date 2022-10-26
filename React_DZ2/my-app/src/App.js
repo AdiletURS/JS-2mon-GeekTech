4ca3c6ea4d40a26ba7fddf1c1edc1e467089cc98
Адилет <110529980+AdiletURS@users.noreply.github.com>
2022-10-26 10:29:48 +0600

    Add files via upload

---
+++ React_DZ2/my-app/src/App.js
@@ -0,0 +1,15 @@
+import AboutUs from "./pages/aboutUs/AboutUs";
+
+
+function App() {
+  const text = {tittle: "hello" ,
+  subtitle: "lorem lorem lorem "
+    }
+    return (
+        <div className="App">
+            <AboutUs info={text}/>
+        </div>
+      )
+
+}
+export default App;
---
+++ React_DZ2/my-app/src/pages/aboutUs/AboutUs.js
@@ -0,0 +1,13 @@
+
+
+const AboutUs = (props) => {
+
+    return (
+        <>
+            <h1>{props.info.tittle}</h1>
+            <h1>{props.info.subtitle}</h1>
+        </>
+    )
+}
+
+export default AboutUs;

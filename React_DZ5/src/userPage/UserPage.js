183c620a3a632202e296cd1aa8469278281f22d9
Адилет <110529980+AdiletURS@users.noreply.github.com>
2022-11-08 21:00:49 +0600

    Add files via upload

---
+++ React_DZ5/src/userPage/UserPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import UserList from "../userList/UserList";
+
+class UserPage extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      userList: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => this.setState({ userList: data }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Userlist</h1>
+        <UserList names={this.state.userList} btn={this.getInfoOne} />
+      </>
+    );
+  }
+}
+export default UserPage;
